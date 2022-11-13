5fdad5cebb7e8f4be8e432b611b099944d6eb01c
jerom <83882350+jerom-1@users.noreply.github.com>
2022-11-12 20:50:45 -0500

    Update LuCrout.py

--- Source/LuCrout.py
+++ Source/LuCrout.py
@@ -1,10 +1,4 @@
-# Entradas:
-# A -> Matriz invertible
-# b -> Vector de constntes
 
-# Salidas:
-# x -> Vector solución
-# L, U -> Matrices de factorizacion
 import numpy as np
 def crout(A,b):
     n = len(A)
@@ -12,17 +6,12 @@ def crout(A,b):
     U = np.eye(n)
 
     for i in range(n):
-        # Matriz L
         for k in range(i, n):
-            # Producto Punto
             sum = 0;
             for j in range(i):
                 sum += (L[k][j] * U[j][i])
             L[k][i] = A[k][i] - sum;
-
-        # Matriz U
         for k in range(i, n):
-            # Producto Punto
             sum = 0;
             for j in range(i):
                 sum += (L[i][j] * U[j][k]);
@@ -31,9 +20,9 @@ def crout(A,b):
     Z = np.linalg.solve(L, b)
     x = np.linalg.solve(U, Z)
 
-    print("Matriz L")
+    print("Matrix L")
     print(L)
-    print("Matriz U")
+    print("Matrix U")
     print(U)
-    print("Solucion")
+    print("Solution")
     print(x)
